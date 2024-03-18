fa35fa7c742dc17295093e9b8e03d070797893fc
nobledsmarts <nobledsmarts@gmail.com>
2024-03-18 16:28:19 +0100

    set up: filament panel

--- app/Models/Admin.php
+++ app/Models/Admin.php
@@ -2,10 +2,26 @@
 
 namespace App\Models;
 
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Panel;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
-class Admin extends Model
+class Admin extends User implements FilamentUser
 {
-    use HasFactory;
+    use HasApiTokens, HasFactory, Notifiable;
+
+    protected $fillable = [
+       'name',
+       'email',
+       'password',
+    ];
+
+    public function canAccessPanel(Panel $panel): bool
+    {
+        return true;
+    }
 }
