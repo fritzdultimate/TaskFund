588b8649a583f70175b03cc5cadf9e30ef2a6482
nobledsmarts <nobledsmarts@gmail.com>
2024-03-19 07:03:15 +0100

    add columns to Levels table

--- database/migrations/2024_03_19_054345_create_levels_table.php
+++ database/migrations/2024_03_19_054345_create_levels_table.php
@@ -13,6 +13,13 @@ return new class extends Migration
     {
         Schema::create('levels', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->decimal('capital');
+            $table->decimal('welcome_bonus')
+                ->comment('cashback');
+            $table->tinyInteger('daily_tasks');
+            $table->string('profit_per_task');
+            $table->boolean('is_automated')->default(false);
             $table->timestamps();
         });
     }
