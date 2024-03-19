2662090bc04aca379160c27d1371b334b0e98406
nobledsmarts <nobledsmarts@gmail.com>
2024-03-19 07:35:37 +0100

    create: filament-resource for UserTask model

---
+++ app/Filament/Resources/UserTaskResource.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\UserTaskResource\Pages;
+use App\Filament\Resources\UserTaskResource\RelationManagers;
+use App\Models\UserTask;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class UserTaskResource extends Resource
+{
+    protected static ?string $model = UserTask::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                //
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                //
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListUserTasks::route('/'),
+            'create' => Pages\CreateUserTask::route('/create'),
+            'edit' => Pages\EditUserTask::route('/{record}/edit'),
+        ];
+    }
+}
