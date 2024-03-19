f9d493e992118f9fa86e6273d4338a9507757fa3
nobledsmarts <nobledsmarts@gmail.com>
2024-03-19 13:14:56 +0100

    create seeder: LevelSeeder

--- config/tasktypes.php
+++ config/tasktypes.php
@@ -1,6 +1,20 @@
 <?php
 
-    return [
-
-    ];
-?>
+return [
+    [
+        'name' => 'Facebook',
+        'image' => '/imgs/icon/facebook.png'
+    ],
+    [
+        'name' => 'Whatsapp',
+        'image' => '/imgs/icon/whatsapp.png'
+    ],
+    [
+        'name' => 'Instagram',
+        'image' => '/imgs/icon/instagram.png'
+    ],
+    [
+        'name' => 'Youtube',
+        'image' => '/imgs/icon/youtube.png'
+    ],
+];
