717d3314e7d9ea656783d722850e203e060f067d
nobledsmarts <nobledsmarts@gmail.com>
2024-03-19 07:18:05 +0100

    set up colunms for userTasks

--- database/migrations/2024_03_19_061057_create_user_tasks_table.php
+++ database/migrations/2024_03_19_061057_create_user_tasks_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('user_tasks', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('task_id')->constrained('tasks')->cascadeOnDelete();
+            $table->foreignId('user_id')->constrained('users')->cascadeOnDelete();
+            $table->enum('status', ['pending', 'processing', 'completed', 'declined']);
+            $table->json('attachment');
             $table->timestamps();
         });
     }
