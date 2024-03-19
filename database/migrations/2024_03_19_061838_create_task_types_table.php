ffefa7f9822b77f636c1ccf576d6406eaa47ad09
nobledsmarts <nobledsmarts@gmail.com>
2024-03-19 07:20:34 +0100

    add columns to tasktypes table

--- database/migrations/2024_03_19_061838_create_task_types_table.php
+++ database/migrations/2024_03_19_061838_create_task_types_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('task_types', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->boolean('is_active')->default(true);
             $table->timestamps();
         });
     }
