31b3dd0becbb197e2cbd518fce47468f4b6017ec
nobledsmarts <nobledsmarts@gmail.com>
2024-03-21 12:28:49 +0100

    add code to seed Tasks table with youtube tasks

--- database/seeders/TaskSeeder.php
+++ database/seeders/TaskSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Task;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,12 @@ class TaskSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $tasks = config('links');
+
+        Task::factory(count($tasks))
+            ->state(function () use (&$tasks) {
+                return array_shift($tasks) ?? [];
+            })
+            ->create();
     }
 }
