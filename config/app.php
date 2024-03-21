02a1e64953b6bb0378010a0e504094e203cd2d9b
nobledsmarts <nobledsmarts@gmail.com>
2024-03-21 10:24:59 +0100

    changes

--- config/app.php
+++ config/app.php
@@ -18,8 +18,6 @@ return [
 
     'name' => env('APP_NAME', 'Laravel'),
 
-   
-
     /*
     |--------------------------------------------------------------------------
     | Application Environment
