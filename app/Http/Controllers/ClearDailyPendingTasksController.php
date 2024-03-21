e42f47129284a0a4528dcbb3b53776c605b5b22d
nobledsmarts <nobledsmarts@gmail.com>
2024-03-21 22:51:50 +0100

    rename controller: ClearDailyPendingTasks to ClearDailyPendingTasksController

--- app/Http/Controllers/ClearDailyPendingTasksController.php
+++ app/Http/Controllers/ClearDailyPendingTasksController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class ClearDailyPendingTasks extends Controller
+class ClearDailyPendingTasksController extends Controller
 {
     /**
      * Handle the incoming request.
