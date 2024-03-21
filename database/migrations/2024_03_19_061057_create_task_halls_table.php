e2c960dcd3f57997109684a143adc14e7a69e8db
nobledsmarts <nobledsmarts@gmail.com>
2024-03-21 23:52:47 +0100

    create migration: add_balance_column_to_user_table

--- database/migrations/2024_03_19_061057_create_task_halls_table.php
+++ database/migrations/2024_03_19_061057_create_task_halls_table.php
@@ -28,6 +28,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('user_tasks');
+        Schema::dropIfExists('task_halls');
     }
 };
