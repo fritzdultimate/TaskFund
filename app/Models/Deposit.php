1f17cfd601e15484224e1998c5de67a49d58ebfb
nobledsmarts <nobledsmarts@gmail.com>
2024-03-23 03:51:46 +0100

    add relationship: transaction to deposit

--- app/Models/Deposit.php
+++ app/Models/Deposit.php
@@ -4,8 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\MorphOne;
 
 class Deposit extends Model
 {
     use HasFactory;
+
+
+    public function transaction(): MorphOne {
+        return $this->morphOne(Transaction::class, 'transactionable');
+    }
 }
