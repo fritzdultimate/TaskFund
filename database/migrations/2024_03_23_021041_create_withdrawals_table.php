f32bf679c7b1005c57fad31e4615a373eec66b78
nobledsmarts <nobledsmarts@gmail.com>
2024-03-23 03:17:27 +0100

    add columns to withdrawals table

--- database/migrations/2024_03_23_021041_create_withdrawals_table.php
+++ database/migrations/2024_03_23_021041_create_withdrawals_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Enums\TransactionStatus;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -15,6 +16,7 @@ return new class extends Migration
             $table->id();
             $table->foreignId('user_id')->constrained('users')->cascadeOnDelete();
             $table->decimal('amount', 20);
+            $table->enum('status', TransactionStatus::values())->default(TransactionStatus::PENDING);
             $table->timestamps();
         });
     }
