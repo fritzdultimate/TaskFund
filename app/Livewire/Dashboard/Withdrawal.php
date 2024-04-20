2115d92a1610bf4976a689216f1fb25b06dd6aa9
nobledsmarts <nobledsmarts@gmail.com>
2024-04-20 12:57:29 +0100

    create component: Withdrawal

---
+++ app/Livewire/Dashboard/Withdrawal.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Dashboard;
+
+use Livewire\Component;
+
+class Withdrawal extends Component
+{
+    public function render()
+    {
+        return view('livewire.dashboard.withdrawal');
+    }
+}
