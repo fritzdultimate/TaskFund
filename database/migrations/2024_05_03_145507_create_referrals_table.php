1367578abe892b8fccdcea25695b01330862aa8c
nobledsmarts <nobledsmarts@gmail.com>
2024-05-03 15:56:43 +0100

    create enum: ReferralLevels

--- database/migrations/2024_05_03_145507_create_referrals_table.php
+++ database/migrations/2024_05_03_145507_create_referrals_table.php
@@ -12,8 +12,19 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('referrals', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
+            Schema::create('referrals', function (Blueprint $table) {
+                $table->id();
+                $table->foreignId('user_id')
+                    ->constrained('users')
+                    ->cascadeOnDelete()
+                    ->cascadeOnUpdate();
+                $table->foreignId('referred_user_id')
+                    ->constrained('users')
+                    ->cascadeOnDelete()
+                    ->cascadeOnUpdate();
+                $table->enum('level', ReferralLevels::values());
+                $table->timestamps();
+            });
         });
     }
 
