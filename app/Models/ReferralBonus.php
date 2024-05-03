dc5c40a5afdd09d3a0abf3a49e1548820c4f1d28
nobledsmarts <nobledsmarts@gmail.com>
2024-05-03 15:59:02 +0100

    add relationships on ReferralBonus model

--- app/Models/ReferralBonus.php
+++ app/Models/ReferralBonus.php
@@ -4,13 +4,26 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\MorphMany;
+use Illuminate\Database\Eloquent\Relations\MorphOne;
 
 class ReferralBonus extends Model
 {
     use HasFactory;
 
-    public function earnings(): MorphMany {
-        return $this->morphMany(Earning::class, 'earnable');
+    public function earning(): MorphOne {
+        return $this->morphOne(Earning::class, 'earnable');
+    }
+
+    public function transaction(): MorphOne {
+        return $this->morphOne(Transaction::class, 'transactionable');
+    }
+
+    public function referred(): BelongsTo {
+        return $this->belongsTo(User::class, 'referred_user_id');
+    }
+    public function user(): BelongsTo {
+        return $this->belongsTo(User::class, 'user_id');
     }
 }
